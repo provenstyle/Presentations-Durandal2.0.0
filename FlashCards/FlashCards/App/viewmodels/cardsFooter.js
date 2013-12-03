1bde9c0f79696a691ea7f35423b92bccb45e8fd6
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-12-03 07:21:32 -0600

    Splitting out the header body and footer for demo

---
+++ FlashCards/FlashCards/App/viewmodels/cardsFooter.js
@@ -0,0 +1,14 @@
+﻿define(['services/logger', 'services/selectedDeck'], function (logger, deck) {
+
+    var random = ko.observable(false),
+        randomChanged = function () {
+            logger.log("Random checkbox value: " + random());
+            deck.pickRandom(random());
+            return true;
+        };
+
+    return {
+        random: random,
+        randomChanged: randomChanged
+    };
+});
---
+++ FlashCards/FlashCards/App/viewmodels/cardsHeader.js
@@ -0,0 +1,18 @@
+﻿define(['durandal/app', 'services/selectedDeck'], function (app, deck) {
+
+    var next = function () {
+        deck.next();
+        app.trigger('updateCard');
+    },
+    previous = function () {
+        deck.previous();
+        app.trigger('updateCard');
+    };
+
+    return {
+        previous: previous,
+        next: next,
+        hasPrevious: deck.hasPrevious,
+        hasNext: deck.hasNext,
+    };
+});
